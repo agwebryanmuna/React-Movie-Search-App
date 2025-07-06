e7794820b04d077815621dba003667c3ef807f37
agwebryanmuna <agwebryan1@gmail.com>
2025-07-06 04:25:13 +0100

    logging api_keys

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -5,6 +5,8 @@ const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
+console.log(PROJECT_ID, DATABASE_ID, COLLECTION_ID);
+
 const client = new Client()
   .setEndpoint('https://fra.cloud.appwrite.io/v1')
   .setProject(PROJECT_ID)
